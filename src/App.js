5fbd447f1fa02117e5c8b460f9b4ace576ed82b7
Alok-Yadav45 <alokyadav@tothebyte.com>
2025-08-18 19:53:38 +0530

    18-assignemt-12

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+function App() {
+    const [todo, setTodo] = useState([
+
+        { id: 1, text: "abc", completed: false },
+        { id: 2, text: "xyz", completed: false }
+
+    ]);
+    const [input, setInput] = useState("");
+
+    function newTOdo() {
+        setTodo((todos) => [...todos,
+            { id: 1, text: input, completed: false }
+        ]);
+        setInput("")
+        
+    }
+    function inputHandler(event) {
+       
+        setInput(event.target.value)
+       
+    }
+
+    return (
+        <div>
+            <h1>Add Todo</h1>
+
+            <input type="text" onChange={inputHandler} value={input}  />
+            <button onEnter={newTOdo}>ADD</button>
+            <ul>
+        {todo.map(item => (
+          <li key={item.id}>
+            {item.text}
+            
+          </li>
+        ))}
+      </ul>
+        </div>
+    );
+}
+
+export default App;
