d66e1f46164e058e3a24f6d9b1da7a55f70cc3f1
Alok-Yadav45 <alokyadav@tothebyte.com>
2025-08-19 17:03:45 +0530

    12 assignment-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import { useState } from "react";
+import { useState  } from "react";
+import "./App.css";
 
 function App() {
     const [todo, setTodo] = useState([
 
-        { id: 1, text: "abc", completed: false },
-        { id: 2, text: "xyz", completed: false }
+        { id: 0, text: "abc", completed: false },
+        { id: 1, text: "xyz", completed: false }
 
     ]);
+    
     const [input, setInput] = useState("");
+    const [error, setError] = useState(false);
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState("");
 
     function newTOdo() {
+
+    if (input.trim() === "" && date.trim () === "" && time.trim() === "") {
+      setError(true);
+      return;
+    }
+
         setTodo((todos) => [...todos,
-            { id: 1, text: input, completed: false }
+            { id: todos.length, text: input, completed: false , date , time }
         ]);
         setInput("")
-        
+        setDate("")
+        setTime("")
+        setError(false)
+
     }
     function inputHandler(event) {
        
         setInput(event.target.value)
-       
+
+    
+
     }
     function handleKeyDown(event) {
     if (event.key === "Enter") {
@@ -27,20 +43,70 @@ function App() {
     } 
 }
 
-    return (
-        <div>
-            <h1>Add Todo</h1>
+  function toggleCompleted(id) {
+    setTodo((todos) =>
+      todos.map((item) =>
+        item.id === id ? { ...item, completed: !item.completed } : item
+      )
+    );
+  }
+  function deleteTodo(id) {
+  setTodo((todos) => todos.filter((item) => item.id !== id));
+}
+  function dateHandler(event) {
+  setDate(event.target.value);
+}
+  function timeHandler(event) {
+  setTime(event.target.value);
+  }
+  
 
-            <input type="text" onChange={inputHandler} value={input} onKeyDown={handleKeyDown} />
-            <button onClick={newTOdo}>ADD</button>
+    return (
+        <div className="App">
+          <div className="input-container">
+            {/* <h1 className="h1">Add Todo</h1> */}
+
+            <input
+    
+            className={error? "input error":"input"}
+            type="text" 
+            onChange={inputHandler} value={input} 
+            onKeyDown={handleKeyDown} />
+            <input
+           className="date-input"
+           type="date"
+           onChange={dateHandler}
+           value={date} />
+            <input
+           className="time-input"
+            type="time"
+            onChange={timeHandler}
+            value={time} />
+            <button className="button" onClick={newTOdo}>Add task</button>
+            
+             </div>
+             
             <ul>
+             
         {todo.map(item => (
           <li key={item.id}>
+            <div className="message">
+              <input
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => toggleCompleted(item.id)}
+            />
+            
             {item.text}
+            <button className="delete-button" onClick={() => deleteTodo(item.id)}>
+          🗑️
+        </button>
+            </div>
             
           </li>
         ))}
       </ul>
+      
         </div>
     );
 }
